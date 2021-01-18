ddbb74b4213c5535fa30f10f1e594ecde7265c98
IvanGent <gentilleivan@gmail.com>
2021-01-18 18:30:13 -0500

    favs done

--- react-app/src/components/Listing/Listing.js
+++ react-app/src/components/Listing/Listing.js
@@ -21,29 +21,30 @@ const liInfo = {
     }
 }
 
-const Favs = {
-    visible: {
-        opacity: 1
-    },
-    hidden: {
-        opacity: 0
-    },
-    hover: {
-        scale: 1.1
-    },
-    startTap: {
-        scale: 0.9
-    },
-    endTap: {
-        scale: 1.2
-    }
-}
+// const Favs = {
+//     visible: {
+//         opacity: 1
+//     },
+//     hidden: {
+//         opacity: 0
+//     },
+//     hover: {
+//         scale: 1.1
+//     },
+//     startTap: {
+//         scale: 0.9
+//     },
+//     endTap: {
+//         scale: 1.2
+//     }
+// }
 
 
 function Listing({ showTab1, showTab2  }) {
     const [res, setRes] = useState([]);
     const [favs, setFavs] = useState([]);
-
+    // const [checkingFavs, setCheckingFvs] = useState([]);
+    const curr = localStorage.getItem('userId')
     const { userId } = useParams()
 
     useEffect(() => {
@@ -51,6 +52,9 @@ function Listing({ showTab1, showTab2  }) {
             const response = await fetch(`/api/users/${userId}`);
             const user = await response.json();
             setFavs(user.favsList)
+            user.favsList.forEach(ele => {
+
+            })
             setRes(user.visitedRestaurants.reverse())
         })()
     }, [userId])
@@ -59,12 +63,37 @@ function Listing({ showTab1, showTab2  }) {
 
     }
 
-    const func = (event, info) => {
-        console.log(event.target.id)
+    const func = async (event) => {
+        // console.log(event.target.id)
         if(event.target.style.fill === 'red') {
             event.target.style.fill = null
+            const res = await fetch('/api/favs/del', {
+                method: "DELETE",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    res_id: event.target.id,
+                    user_id: curr
+                })
+            })
+            const data = await res.json()
+            console.log(data);
+
         } else {
             event.target.style.fill = 'red'
+            const res = await fetch('/api/favs/', {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    res_id: event.target.id,
+                    user_id: curr
+                })
+            })
+            const data = await res.json();
+            console.log(data)
         }
     }
 
@@ -91,7 +120,14 @@ function Listing({ showTab1, showTab2  }) {
                         <AnimatePresence>
                         <motion.div initial={{opacity: 0}} animate={{opacity: 1}}>
                             <ul>
-                            {res.map((ele, i) => (
+                            {res.map((ele, i) =>{
+                                let fill = null
+                                favs.forEach(ele2 => {
+                                    if(ele.restaurant.id === ele2.restaurant.id) {
+                                        fill = 'red'
+                                    }
+                                })
+                             return (
                                 <motion.li
                                   variants={liInfo}
                                   initial='hidden'
@@ -105,22 +141,8 @@ function Listing({ showTab1, showTab2  }) {
                                         <img src={ele.restaurant.logo} alt='logo' />
                                     </div>
                                     <span id='line'>-</span><span>{ele.restaurant.name}</span>
-                                    {/* <motion.img
-                                        id={i}
-                                        className='notFav'
-                                        variants={Favs}
-                                        initial='hidden'
-                                        animate='visible'
-                                        whileTap='startTap'
-                                        whileHover='hover'
-                                        onTap={func}
-                                        src={process.env.PUBLIC_URL + '/Fav.png'}
-                                        width={20}
-                                        height={20}
-                                        // onClick={FavHandle}
-                                    ></motion.img> */}
                                         <motion.svg
-                                            id={i}
+                                            id={ele.restaurant.id}
                                             variants={svgVar}
                                             // enable-background="new 0 0 24 24" 
                                             // enableBackground='red'
@@ -128,7 +150,7 @@ function Listing({ showTab1, showTab2  }) {
                                             animate='visible'
                                             whileTap='tap'
                                             whileHover='hover'
-                                            style
+                                            style={{fill: fill}}
                                             onClick={func}
                                             version="1.0" viewBox="0 0 24 24" 
                                             xmlns="http://www.w3.org/2000/svg">
@@ -136,7 +158,7 @@ function Listing({ showTab1, showTab2  }) {
                                         </motion.svg>
                                 </div>
                                 </motion.li>
-                            ))}
+                            )})}
                             </ul>
                         </motion.div>
                         </AnimatePresence>
@@ -169,8 +191,27 @@ function Listing({ showTab1, showTab2  }) {
                                         exit='exit'
                                         key={ele.id}
                                     >
-                                        <img src={ele.restaurant.logo} alt='logo' />
-                                        - <span>{ele.restaurant.name}</span>
+                                        <div className='innerLi'>
+                                            <div>
+                                                <img src={ele.restaurant.logo} alt='logo' />
+                                            </div>
+                                            <span id='line'>-</span><span>{ele.restaurant.name}</span>
+                                            <motion.svg
+                                                id={ele.restaurant.id}
+                                                variants={svgVar}
+                                                // enable-background="new 0 0 24 24" 
+                                                // enableBackground='red'
+                                                initial='hidden'
+                                                animate='visible'
+                                                whileTap='tap'
+                                                whileHover='hover'
+                                                style={{ fill: 'red' }}
+                                                onClick={func}
+                                                version="1.0" viewBox="0 0 24 24"
+                                                xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M16.4 6c2 0 3.6 1.6 3.6 3.6s-3.9 6.4-8 9.8c-4.1-3.5-8-7.9-8-9.8C4 7.6 5.6 6 7.6 6 10 6 12 9 12 9s1.9-3 4.4-3m0-2c-1.8 0-3.4.9-4.4 2.3C11 4.9 9.4 4 7.6 4 4.5 4 2 6.5 2 9.6 2 14 12 22 12 22s10-8 10-12.4C22 6.5 19.5 4 16.4 4z" />
+                                            </motion.svg>
+                                        </div>
                                     </motion.li>
                                 ))}
                             </ul>
