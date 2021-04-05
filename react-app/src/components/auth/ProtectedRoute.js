654f48fc160b6300cfe1a9e9a17d73dfffca8505
IvanGent <gentilleivan@gmail.com>
2021-04-05 13:52:53 -0400

    updating protected route

--- react-app/src/components/auth/ProtectedRoute.js
+++ react-app/src/components/auth/ProtectedRoute.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import {Route, Redirect} from 'react-router-dom';
+// import { authenticate } from '../../services/auth';
 
 const ProtectedRoute = (props) => {
     if(!props.authenticated) {
         return <Redirect to ="/login"/>
     }
 
-    return {
+    return (
         <Route {...props}/>
-    };
+    );
 };
 
 export default ProtectedRoute;
