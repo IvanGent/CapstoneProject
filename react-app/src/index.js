733791a50fdf309c6d235ac4c61a8a11da844bfc
IvanGent <gentilleivan@gmail.com>
2021-04-05 17:28:24 -0400

    updating

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRoute } from 'react-router-dom';
+import import configureStore from './store';
 import './index.css';
 import App from './App';
+
 
 ReactDOM.render(
   <React.StrictMode>
