64fd051e2e9bd4b279af217c64b150dbf7e887b4
IvanGent <gentilleivan@gmail.com>
2021-04-06 13:01:36 -0400

    setting up session user

---
+++ react-app/src/store/session.js
@@ -0,0 +1,10 @@
+const SET_USER = 'session/setUser';
+const REMOVE_USER = 'session/removeUser';
+
+const setUser = (user) => {
+    return {
+        type: SET_USER,
+        payload: user,
+    };
+};
+
