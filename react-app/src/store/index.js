dd567b7dc1cd44e33b98c874d01a288a0ac34a75
IvanGent <gentilleivan@gmail.com>
2021-04-06 12:45:42 -0400

    updating index for redux store

--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -3,9 +3,23 @@ import thunk from 'redux-thunk';
 // import sessionReducer from './session';
 
 const rootReducer = combineReducers({
-    session: sessionReducer,
+    // session: sessionReducer,
 })
 
-let enhancer;
 
-if(process.env.NODE_ENV)
+///// THIS IS FOR REDUX LOGGER
+// let enhancer;
+
+// if(process.env.NODE_ENV === 'production') {
+//     enhancer = applyMiddleware(thunk);
+// } else {
+//     const composeEnhancers = 
+//       window.__REDUX_DEVTOOLS_EXTENSIONS_COMPOSE__ || compose;
+//     enhancer = composeEnhancers(applyMiddleware(thunk, logger));
+// }
+
+const configureStore = (preloadedState) => {
+    return createStore(rootReducer, preloadedState)
+}
+
+export default configureStore;
